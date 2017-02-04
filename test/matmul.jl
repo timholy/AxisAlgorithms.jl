74ed2358b5c0c55f042f4dea9ebfae0470699d35
Jan Weidner <jw3126@gmail.com>
2017-02-04 16:53:15 +0100

    replace @test_approx_eq

--- test/matmul.jl
+++ test/matmul.jl
@@ -8,11 +8,11 @@ for M in (rand(m,n), sprand(m,n,0.2))
         sz[dim] = m
         dest2 = rand(sz...)
         AxisAlgorithms.A_mul_B_md!(dest2, M, src, dim)
-        @test_approx_eq dest1 dest2
+        @test dest1 ≈ dest2
         if !issparse(M)
             rand!(dest2)
             AxisAlgorithms.A_mul_B_perm!(dest2, M, src, dim)
-            @test_approx_eq dest1 dest2
+            @test dest1 ≈ dest2
         end
     end
 end
@@ -41,11 +41,11 @@ for dim = 1:3
     M = fill(3.2,1,1)
     dest1 = mapslices(b->M*b, src, dim)
     dest2 = AxisAlgorithms.A_mul_B_md(M, src, dim)
-    @test_approx_eq dest1 dest2
+    @test dest1 ≈ dest2
     sz[dim] = 2
     src = rand(sz...)
     M = rand(2,2)
     dest1 = mapslices(b->M*b, src, dim)
     dest2 = AxisAlgorithms.A_mul_B_md(M, src, dim)
-    @test_approx_eq dest1 dest2
+    @test dest1 ≈ dest2
 end
--- test/tridiag.jl
+++ test/tridiag.jl
@@ -8,5 +8,5 @@ for dim = 1:3
     dest1 = mapslices(x->A_ldiv_B!(F, x), copy(src), dim)
     dest2 = similar(src)
     AxisAlgorithms.A_ldiv_B_md!(dest2, F, src, dim)
-    @test_approx_eq dest1 dest2
+    @test dest1 ≈ dest2
 end
